fb71237964569d3b27fb4bbff6abcb8f39e59a8f
Cindy <cindy.lecomte59@gmail.com>
2024-04-02 15:06:03 +0200

    exo 3 - A voir

---
+++ exo3/script.js
@@ -0,0 +1,16 @@
+let nombreUn = document.getElementById("numberOne")
+
+let nombreDeux = document.getElementById("numberTwo")
+
+let resultNumber = nombreUn.value * nombreDeux.value
+
+console.log(resultNumber)
+
+//let button = document.querySelector("button")
+
+
+//button.addEventListener("click", (event) => {   
+    //console.log(resultNumber)
+//})
+
+// Ca marche en faisant le bouton rafraichir mais pas en cliquant sur le bouton
