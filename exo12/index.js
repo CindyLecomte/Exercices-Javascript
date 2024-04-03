25a4654532d2435b910b49c7c7dadb3b4c3e4146
Cindy <cindy.lecomte59@gmail.com>
2024-04-03 16:23:23 +0200

    aaaaaaaaaaaaaaaaaaaaaah

---
+++ exo12/index.js
@@ -0,0 +1,17 @@
+const tableau = [
+    1,12,25,30
+]
+
+//let counter = 0;    
+
+//for (let elt of tableau){
+
+   // if(elt >= 10) {
+      //  console.log("ok")
+     //   counter = counter +1
+        
+   // } else {
+    //    console.log("erreur")
+  //  }
+//}
+
