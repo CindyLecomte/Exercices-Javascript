f2c4651420446cafc42341855edfbeafc336d178
Cindy <cindy.lecomte59@gmail.com>
2024-04-03 14:27:27 +0200

    exo 2 terminé

--- exo2/script.js
+++ exo2/script.js
@@ -6,5 +6,5 @@ let inputFirstname = document.getElementById("firstname")
 let button = document.querySelector("button")
 
 button.addEventListener("click", (event) => {   
-    console.log(inputName.value + inputFirstname.value) 
+    console.log(inputName.value , inputFirstname.value) 
 })
