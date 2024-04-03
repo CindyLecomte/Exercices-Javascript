f46d82c9f44e905a358de3db01f5c5fe9c2b297d
Cindy <cindy.lecomte59@gmail.com>
2024-04-03 09:25:27 +0200

    exo5 - A voir

---
+++ exo5/script.js
@@ -0,0 +1,25 @@
+const numberOne = document.getElementById("numberOne")
+
+const numberTwo = document.getElementById("numberTwo")
+
+const numberThree = document.getElementById("numberThree")
+
+const button = document.getElementById("button")
+
+if (numberOne.value > numberTwo.value && numberOne.value > numberThree.value) {
+    console.log(numberOne.value)
+ } else if (numberTwo.value > numberOne.value && numberTwo.value > numberThree.value) {
+    console.log(numberTwo.value)
+ } else {
+   console.log(numberThree.value)
+}
+
+
+//button.addEventListener("click", (event) => {
+  //  if (numberOne.value > numberTwo.value && numberOne.value > numberThree.value) {
+    //   console.log(numberOne.value)
+    //} else if (numberTwo.value > numberOne.value && numberTwo.value > numberThree.value) {
+     //  console.log(numberTwo.value)
+   // } else {
+    //  console.log(numberThree.value)
+  // }}) 
