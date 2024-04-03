06416c2c62d2b443cbc5fea5a44c548546c3b5b3
Cindy <cindy.lecomte59@gmail.com>
2024-04-03 14:47:26 +0200

    exo 6 terminé

--- exo6/script.js
+++ exo6/script.js
@@ -1,19 +1,12 @@
 let note = document.getElementById("note")
 let button = document.getElementById("button")
 
-//if(note.value >=10) {
- //   console.log("Validé")
-//} else {
- //   console.log("Non validé")
-//}
+
 
 button.addEventListener("click", (event) => {
-    console.log("coucou")   // ???
     if(note.value >=10) {
         console.log("Validé")
     } else {
         console.log("Non validé")
     }
 })
-// Sans le console.log("coucou"), rien ne s'affiche en console.
-// Avec le console.log, le "coucou" ne s'affiche pas mais affiche ma condition en console selon le nombre saisi.
