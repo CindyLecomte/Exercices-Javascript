2354685e9176090012cd827e78628bc15e203627
Cindy <cindy.lecomte59@gmail.com>
2024-04-05 13:42:20 +0200

    exo 18bis

---
+++ exo18bis/index.js
@@ -0,0 +1,25 @@
+function concat(tableauA, tableauB){
+    const tableauC = []
+
+    for (let elt of tableauA){
+        tableauC.push(elt)
+    }
+    for (let elt of tableauB){
+        tableauC.push(elt)
+    }
+
+    return (
+        tableauA + tableauB
+    )
+}
+
+const tableauUn = [
+    1,2,3,4
+]
+
+const tableauDeux = [
+    5,6,7,8
+]
+
+const tableauTrois = concat(tableauUn, tableauDeux)
+console.log(tableauTrois)
