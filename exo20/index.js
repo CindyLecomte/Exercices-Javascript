ad46fc30e10e6a883c2af8652d8fadb7b52cda47
Cindy <cindy.lecomte59@gmail.com>
2024-04-05 15:46:22 +0200

    rfd

---
+++ exo20/index.js
@@ -0,0 +1,8 @@
+function repeteBonjour(nombreDeFoisQueJeRepete){
+    let counter = 0;
+    while(counter < nombreDeFoisQueJeRepete) {
+        counter= counter +1
+    }
+}
+
+console.log(repeteBonjour(3))
