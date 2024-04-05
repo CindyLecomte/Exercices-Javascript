bf7cedcce9a8b983707491b0dfe11d8c3b7c3fb2
Cindy <cindy.lecomte59@gmail.com>
2024-04-05 15:51:02 +0200

    regarde

--- exo20/index.js
+++ exo20/index.js
@@ -2,7 +2,7 @@ function repeteBonjour(nombreDeFoisQueJeRepete){
     let counter = 0;
     while(counter < nombreDeFoisQueJeRepete) {
         counter= counter +1
-        console.log("Bonjour", counter)
+        console.log("Bonjour")
     }
 }
 
