01dfd36a6d00753b6895d889a8eb4e00ccefb495
Cindy <cindy.lecomte59@gmail.com>
2024-04-08 09:01:44 +0200

    exo 2

---
+++ revisions/exo2/index.js
@@ -0,0 +1,9 @@
+const inputUn = document.getElementById("inputUn")
+
+const inputDeux = document.getElementById("InputDeux")
+
+const button = document.getElementById("button")
+
+button.addEventListener("click", (event) => {
+    console.log(inputUn.value , inputDeux.value)
+})
