c0efb7884346d5f4f61efe086e472fe1f13bd6ec
Cindy <cindy.lecomte59@gmail.com>
2024-04-08 11:17:55 +0200

    exo 14

--- revisions/exo14/index.js
+++ revisions/exo14/index.js
@@ -2,8 +2,17 @@ const tableau = [
     1,2,3,4,5
 ]
 
-let counter = 0
-for (let note of tableau){
-    moyenne = note += counter
-console.log(moyenne)
+let accu = 0    //On déclare le compter à 0
+
+for (let elt of tableau){   //Pour chaque élément du tableau
+    accu = accu + elt   //On redéclare accu en disant qu'on veut l'ancienne version d'accu + l'élément de notre tableau
+                        // Donc: 
+                        // accu = 0 +1 = 1
+                        // accu = 1+2 = 3
+                        // etc ...
 }
+
+console.log(accu)   //on peut console.log accu puisqu'on l'a déclaré avant la boucle
+
+moyenne = accu / tableau.length
+console.log(moyenne)
