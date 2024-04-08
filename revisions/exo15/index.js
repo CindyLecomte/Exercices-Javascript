43db55e1bdff40fc71deb4b0853e59b8ab900bea
Cindy <cindy.lecomte59@gmail.com>
2024-04-08 11:26:42 +0200

    exo 15

---
+++ revisions/exo15/index.js
@@ -0,0 +1,19 @@
+const tableauA = [
+    1,5,7
+]
+
+const tableauB = [
+    2,8,6
+]
+
+const tableauC = []
+
+for (let elt of tableauA){
+    tableauC.push(elt)
+}
+
+for(let elt of tableauB){
+    tableauC.push(elt)
+}
+
+console.log(tableauC)
